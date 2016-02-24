68dca03273ea1f1c36a41bc4bd80ce90ad8bf665
wangyan <wyvernnot@gmail.com>
2016-02-24 19:43:25 +0800

    read from environment BASE_URL and mock http

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,9 +4,16 @@
 
 import * as API from '../lib/index';
 import {ok} from 'assert';
+import nock from 'nock';
 
 describe('API', ()=> {
   it('should be defined', ()=> {
-    ok(Object.keys(API).length > 1);
+    nock(process.env.BASE_URL).get(/^\/overview/).reply(200, {
+      code: 0,
+      result: {
+        datum: []
+      }
+    });
+    return API.hosts()
   });
 });
